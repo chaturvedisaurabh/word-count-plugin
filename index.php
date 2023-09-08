c75af0b04213ad1a25ff869cb83c3658b5ea3569
chaturvedisaurabh <110327955+chaturvedisaurabh@users.noreply.github.com>
2023-09-08 11:46:17 +0530

    Create index.php
    added basic functionality code that shows a total word count for post

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Word Count WOS
+ * Description: Adds a column to all posts page showing each post’s word count.
+ * Version: 1.0
+ * Author: Saurabh Chaturvedi
+ */
+
+// Add a column to the posts table
+function add_word_count_column($columns) {
+    $columns['word_count'] = 'Word Count';
+    return $columns;
+}
+add_filter('manage_posts_columns', 'add_word_count_column');
+
+// Populate the word count column
+function populate_word_count_column($column, $post_id) {
+    if ($column == 'word_count') {
+        $post = get_post($post_id);
+        $content = $post->post_content;
+        $word_count = str_word_count(strip_tags($content));
+        echo $word_count;
+    }
+}
+add_action('manage_posts_custom_column', 'populate_word_count_column', 10, 2);
